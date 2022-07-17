f0e47a40fd9bc8274d08d7ec7059bfa0e5f4709c
pwho <patrick@patrickwho.me>
2022-07-17 16:08:26 +0200

    Add bootstrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,9 @@
         @routes
         @vite('resources/js/app.js')
         @inertiaHead
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
