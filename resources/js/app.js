a9d5f85d2fc31b6e405fa7154bf514f6e25f8a17
pwho <patrick@patrickwho.me>
2022-07-21 15:28:47 +0200

    Change out Vite for Mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,15 @@
 import './bootstrap';
-import '../css/app.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,8 @@
 
         <!-- Scripts -->
         @routes
-        @vite('resources/js/app.js')
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="{{ mix('js/app.js') }}" defer></script>
         @inertiaHead
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
