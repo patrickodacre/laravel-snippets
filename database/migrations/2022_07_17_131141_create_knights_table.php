b40993a262204f72e69e80917042821c92104612
pwho <patrick@patrickwho.me>
2022-07-21 15:29:05 +0200

    Remove color from Knights migration to keep it simple

--- database/migrations/2022_07_17_131141_create_knights_table.php
+++ database/migrations/2022_07_17_131141_create_knights_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('color');
 
             $table->timestamps();
             $table->softDeletes();
