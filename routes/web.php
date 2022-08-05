df47b73b1ea77f89063c33b0d57a94c43df764d3
pwho <patrick@patrickwho.me>
2022-08-05 15:52:49 +0200

    Remove unnecessary auth checks for routes.

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,9 @@ Route::get('/', function () {
 });
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+    // 'auth:sanctum',
+    // config('jetstream.auth_session'),
+    // 'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
